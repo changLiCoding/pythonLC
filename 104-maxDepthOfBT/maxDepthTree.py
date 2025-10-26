11739442d9d6bdfade432d27001b5ad4076b78da
Chang Li <lichang9488@gmail.com>
2025-10-26 14:31:15 -0400

    Trees

--- 104-maxDepthOfBT/maxDepthTree.py
+++ 104-maxDepthOfBT/maxDepthTree.py
@@ -1,9 +1,23 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # if not root:
+        #   return 0
+        # res = 0
+        # q = deque([root])
+        # while q:
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     res += 1
+        # return res
+
         if not root:
-          return 0
-        res = 0
+            return 0
         q = deque([root])
+        res = 0
         while q:
             for i in range(len(q)):
                 node = q.popleft()
---
+++ 110-balancedBinaryTree/balancedTree.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return [True, 0]
+            l, r = dfs(root.left), dfs(root.right)
+            balanced = l[0] and r[0] and abs(l[1] - r[1]) <= 1
+            return [balanced, 1 + max(l[1], r[1])]
+
+        return dfs(root)[0]
